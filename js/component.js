cec58835c2eaf711f90335eb0e114edcac7c9797
Yi-Min Tsai <Yi-Min.Tsai@quantatw.com>
2015-02-02 06:32:56 +0800

    Initial version

---
+++ js/component.js
@@ -0,0 +1,33 @@
+/* jQuery for skill bars */
+$(document).ready(function() {
+	
+	jQuery('.skillbar').each(function() {
+		jQuery(this).appear(function() {
+			jQuery(this).find('.skillbar-bar').animate({
+				width : jQuery(this).attr('data-percent')
+			}, 1700, "easeOutCirc");
+		});
+	});
+});
+
+/* JQuery Owl slider */
+$(document).ready(function() {
+
+	$("#owl-demo").owlCarousel({
+
+		navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem : true
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1,
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+
+	});
+
+});
+
