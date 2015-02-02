2de4aa3ad6f11fdf65836b123e51cd845dca472c
yimintsai <yimintsai@gmail.com>
2015-02-02 16:21:08 +0800

    Add owl carousel

--- js/component.js
+++ js/component.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
 	$("#owl-demo").owlCarousel({
 
-		navigation : true, // Show next and prev buttons
+		navigation : false, // Show next and prev buttons
 		slideSpeed : 300,
 		paginationSpeed : 400,
 		singleItem : true
