dfea1ca920d1777bed63d6f55d9e6a4be1e870d0
Yi-Min Tsai <Yi-Min.Tsai@quantatw.com>
2015-02-15 20:10:06 +0800

    modify theme

--- js/component.js
+++ js/component.js
@@ -1,12 +1,36 @@
 /* jQuery for skill bars */
 $(document).ready(function() {
-	
+
 	jQuery('.skillbar').each(function() {
 		jQuery(this).appear(function() {
 			jQuery(this).find('.skillbar-bar').animate({
 				width : jQuery(this).attr('data-percent')
 			}, 1700, "easeOutCirc");
 		});
+	});
+});
+
+/* JQuery for honor/skill logo*/
+$(document).ready(function() {
+
+	totalloges = 2;
+
+	$('.honor-panel-heading').appear(function() {
+		$('.honor-panel-logo').fadeIn(2000);
+
+		/*
+		 for (var x = 1; x <= totalloges; x++) {
+		 $('#hpl-' + x + ' i').fadeIn('slow');
+		 //$it.addClass(honor_logo[x-1]);
+		 //$it.fadeIn("slow");
+		 //$it.fadeOut('slow');
+		 }
+		 */
+
+	});
+
+	$('.skill-panel-heading').appear(function() {
+		$('.skill-panel-logo').fadeIn(2000);
 	});
 });
 
