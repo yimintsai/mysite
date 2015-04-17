3d601d4f1e7d5caade4932260f40dd6195f80155
Yi-Min Tsai <Yi-Min.Tsai@quantatw.com>
2015-04-17 20:48:19 +0800

    Add jquery-ui.js

--- js/component.js
+++ js/component.js
@@ -1,17 +1,46 @@
-/* jQuery for skill bars */
+/* JQuery for timeline */
 $(document).ready(function() {
 
-	jQuery('.skillbar').each(function() {
-		jQuery(this).appear(function() {
-			jQuery(this).find('.skillbar-bar').animate({
-				width : jQuery(this).attr('data-percent')
-			}, 1600, "easeOutCirc");
+	$('.cbp_tmlabel').each(function() {
+		$(this).appear(function() {
+
+			$(this).css({
+				opacity : 0
+			}).animate({
+				opacity : 1.0
+			}, 1600);
 		});
 	});
+
+    /*
+	$('.cbp_tmicon').each(function() {
+		$(this).appear(function() {
+			$(this).effect("bounce", {
+				direction : 'down',
+				times : 1
+			}, 200);
+
+		});
+	});
+	*/
 });
 
+/* jQuery for skill bars */
+/*
+ $(document).ready(function() {
+
+ jQuery('.skillbar').each(function() {
+ jQuery(this).appear(function() {
+ jQuery(this).find('.skillbar-bar').animate({
+ width : jQuery(this).attr('data-percent')
+ }, 1600, "easeOutCirc");
+ });
+ });
+ });
+ */
+
 $(document).ready(function() {
-	
+
 	$('.skill-chart').each(function() {
 		$(this).appear(function() {
 			$(this).easyPieChart({
@@ -21,7 +50,7 @@ $(document).ready(function() {
 				lineCap : 'square',
 				scaleColor : false,
 				trackColor : '#e6e8ed',
-				size : 128				
+				size : 128
 			});
 		});
 	});
@@ -43,24 +72,23 @@ $(document).ready(function() {
 
 });
 
-
 /*
-$(document).ready(function() {
+ $(document).ready(function() {
 
-	totalpanels = 2;
-	var max_panelheight = -1;
+ totalpanels = 2;
+ var max_panelheight = -1;
 
-	for (var i = 1; i <= totalpanels; i++) {
-		var panelheight = parseInt($('#hp-' + i).find('.honor-panel-content').css('height'));
-		if (panelheight > max_panelheight)
-			max_panelheight = panelheight;
-	}
+ for (var i = 1; i <= totalpanels; i++) {
+ var panelheight = parseInt($('#hp-' + i).find('.honor-panel-content').css('height'));
+ if (panelheight > max_panelheight)
+ max_panelheight = panelheight;
+ }
 
-	for (var i = 1; i <= totalpanels; i++) {
-		$('#hp-' + i).find('.honor-panel-content').css('height', max_panelheight);
-	}
-});
-*/
+ for (var i = 1; i <= totalpanels; i++) {
+ $('#hp-' + i).find('.honor-panel-content').css('height', max_panelheight);
+ }
+ });
+ */
 
 /* JQuery Owl slider */
 $(document).ready(function() {
@@ -71,7 +99,7 @@ $(document).ready(function() {
 		slideSpeed : 300,
 		paginationSpeed : 400,
 		singleItem : true,
-		autoPlay: true
+		autoPlay : true
 
 		// "singleItem:true" is a shortcut for:
 		// items : 1,
