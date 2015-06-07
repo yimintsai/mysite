f2d60de0eb6e0322d9c4f7ed946eacfb3044b241
Yi-Min Tsai <Yi-Min.Tsai@quantatw.com>
2015-06-07 17:09:35 +0800

    Change mailing email

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,40 +20,36 @@ $(function() {
 				firstName = name.split(' ').slice(0, -1).join(' ');
 			}
 			$.ajax({
-			//url: "http://video.ee.ntu.edu.tw/~firstpeople/mail/contact_me.php",
-			//url: "././mail/contact_me.php",
-			url : "//formspree.io/firstpeople1902@gmail.com",
-			type : "POST",
-			/*
-			 data : {
-			 name : name,
-			 phone : phone,
-			 email : email,
-			 message : message
-			 },*/
-			data : {
-			message : "hello!"
-			},
-			dataType : "json",
-			cache : false,
-			success : function() {
-				// Success message
-				$('#success').html("<div class='alert alert-success'>");
-				$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-				$('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
-				$('#success > .alert-success').append('</div>');
+				//url: "././mail/contact_me.php",
+				url : "//formspree.io/yimintsai@gmail.com",
+				method : "POST",
+				data : {
+					name : name,
+					phone : phone,
+					email : email,
+					message : message
+				},
+				dataType : "json",
+				cache : false,
+				success : function() {
+					// Success message
+					$('#success').html("<div class='alert alert-success'>");
+					$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+					$('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+					$('#success > .alert-success').append('</div>');
 
-				//clear all fields
-				$('#contactForm').trigger("reset");
-			}, error : function() {
-				// Fail message
-				$('#success').html("<div class='alert alert-danger'>");
-				$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-				$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-				$('#success > .alert-danger').append('</div>');
-				//clear all fields
-				$('#contactForm').trigger("reset");
-			},
+					//clear all fields
+					$('#contactForm').trigger("reset");
+				},
+				error : function() {
+					// Fail message
+					$('#success').html("<div class='alert alert-danger'>");
+					$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+					$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+					$('#success > .alert-danger').append('</div>');
+					//clear all fields
+					$('#contactForm').trigger("reset");
+				},
 			})
 
 		},
