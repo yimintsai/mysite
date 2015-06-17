220658c58add93975c6686ef2c2c4cbc256dae82
Yi-Min Tsai <Yi-Min.Tsai@quantatw.com>
2015-06-17 14:47:59 +0800

    Add portfolio

--- js/component.js
+++ js/component.js
@@ -112,28 +112,6 @@ $(document).ready(function() {
 
 });
 
-/*
- function initialize() {
- var mapOptions = {
- center: new google.maps.LatLng(28.1823294, -82.352912),
- zoom: 9,
- mapTypeId: google.maps.MapTypeId.HYBRID,
- scrollwheel: false,
- draggable: false,
- panControl: true,
- zoomControl: true,
- mapTypeControl: true,
- scaleControl: true,
- streetViewControl: true,
- overviewMapControl: true,
- rotateControl: true,
- };
- var map = new google.maps.Map(document.getElementById("gmap"), mapOptions);
- }
- google.maps.event.addDomListener(window, 'load', initialize);
-
- */
-
 /* Google Map */
 $(document).ready(function() {
 
