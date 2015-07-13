0d6799a69df7af54dfe958553f1b46855541e8d8
yimintsai <yimintsai@gmail.com>
2015-07-13 15:21:00 +0800

    Add cs fig

--- js/component.js
+++ js/component.js
@@ -130,7 +130,26 @@ $(document).ready(function() {
 		// itemsMobile : false
 
 	});
+	
+	$("#cs-gallery").owlCarousel({
 
+		navigation : false, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem : true,
+		autoPlay : false,
+		navigation : true,
+		navigationText : ["<", ">"],
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1,
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+
+	});
+	
 	$("#vs-gallery").owlCarousel({
 
 		navigation : false, // Show next and prev buttons
